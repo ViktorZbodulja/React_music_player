c676633f9f224f6513f8aa6a6cf9fec0a1a71650
Viktor Zbodulja <viktorzbodulja@gmail.com>
2023-02-25 22:28:42 +0100

    master 3. commit volume fix, img add

--- src/data.js
+++ src/data.js
@@ -1,11 +1,19 @@
 import { v4 as uuidv4 } from "uuid";
+import BeaverCreek from "./img/BeaverCreek.jpg";
+import Rewind from "./img/Rewind.jpg";
+import Badlands from "./img/Badlands.jpg";
+import Ambleside from "./img/Ambleside.jpg";
+import Persist from "./img/Persist.jpg";
+import Oasis from "./img/Oasis.jpg";
+import Fallin from "./img/Fallin.jpg";
+import Daydreams from "./img/Daydreams.jpg";
+import Destination from "./img/Destination.jpg";
 
 function chillHop() {
   return [
     {
       name: "Beaver Creek",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      cover: `${BeaverCreek}`,
       artist: "Aso, Middle School, Aviino",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=8078",
       color: ["#ECBB74", "#2ab3bf"],
@@ -15,8 +23,7 @@ function chillHop() {
     {
       name: "Rewind",
       artist: "SwuM, Afternoon Bike Ride",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2022/01/d7c0bd347f56540babd9dd85454b93a89df84a15-1024x1024.jpg",
+      cover: `${Rewind}`,
       id: uuidv4(),
       color: ["#F58863", "#5F434A"],
       audio: "https://mp3.chillhop.com/serve.php/?mp3=30134",
@@ -24,8 +31,7 @@ function chillHop() {
     },
     {
       name: "Badlands",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2021/06/23b2ff959731b56ea8545828b462311e8b1a2bcc-1024x1024.jpg",
+      cover: `${Badlands}`,
       artist: "Hanz, Makzo",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=20126",
       color: ["#F3AF8E", "#C6A7D5"],
@@ -35,8 +41,7 @@ function chillHop() {
     {
       name: "Ambleside",
       artist: "Aarigod",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2021/02/7f102bdde417f6ead9a120b2b931449e5c12b4da-1024x1024.jpg",
+      cover: `${Ambleside}`,
       id: uuidv4(),
       color: ["#ADC772", "#3D4140"],
       audio: "https://mp3.chillhop.com/serve.php/?mp3=12999",
@@ -44,8 +49,7 @@ function chillHop() {
     },
     {
       name: "Persist",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/05/16bcbe48680098798af04f176daae3693839238d-1024x1024.jpg",
+      cover: `${Persist}`,
       artist: "Invention",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=8291",
       color: ["#F79AAD", "#5771B0"],
@@ -54,8 +58,7 @@ function chillHop() {
     },
     {
       name: "Oasis",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/11/f78c39b4bb6313ddd0354bef896c591bfb490ff8-1024x1024.jpg",
+      cover: `${Oasis}`,
       artist: "Hanz, Makzo",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=11768",
       color: ["#FBBC69", "#6F4B3B"],
@@ -64,8 +67,7 @@ function chillHop() {
     },
     {
       name: "Fallin'",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/58028b4ad62050072228e4c833916e5512772399-1024x1024.jpg",
+      cover: `${Fallin}`,
       artist: "Ruck P, Jaz Lund",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=20561",
       color: ["#CB8794", "#2D2F4A"],
@@ -74,8 +76,7 @@ function chillHop() {
     },
     {
       name: "Under the City Stars",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
+      cover: `${BeaverCreek}`,
       artist: "Aso, Middle School, Aviino",
       audio: "https://mp3.chillhop.com/serve.php/?mp3=10074",
       color: ["#ECBB74", "#2ab3bf"],
@@ -85,8 +86,7 @@ function chillHop() {
     {
       name: "Daydreams",
       artist: "Evil Needle",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/2c3bd458bfb0713c89f991d1ce469523e95e3b53-1024x1024.jpg",
+      cover: `${Daydreams}`,
       id: uuidv4(),
       color: ["#EF9D82", "#683A75"],
       audio: "https://mp3.chillhop.com/serve.php/?mp3=8883",
@@ -95,8 +95,7 @@ function chillHop() {
     {
       name: "Destination",
       artist: "Ruck P, Djemeia",
-      cover:
-        "https://chillhop.com/wp-content/uploads/2020/07/9630fcab71c6dc1e9ad8140a03dcab54b8b98098-1024x1024.jpg",
+      cover: `${Destination}`,
       id: uuidv4(),
       color: ["#ED8DB6", "#695C9D"],
       audio: "https://mp3.chillhop.com/serve.php/?mp3=8157",
